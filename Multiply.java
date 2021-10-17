583bec574c5e5880a46455c79139cfa5ea58d0e3
Vipin Kumar Sharma <20vipin09@gmail.com>
2021-10-17 08:36:49 +0530

    added multiply java program

---
+++ Multiply.java
@@ -0,0 +1,16 @@
+package LetsLearnGit;
+
+public class Multiply {
+
+	public static void main(String[] args) {
+		
+		int a = 2;
+		int b = 3;
+		int mul = a*b;
+		
+		System.out.println("Multiplication: "+mul);
+		
+
+	}
+
+}
